c6e910cd03ed7f75e378ee1761433e2e675ba9ca
alexmi94 <alexandrecance94@gmail.com>
2021-05-21 18:44:04 +0200

    add script BBD

---
+++ BBD_client_ligue.sql
@@ -0,0 +1,5 @@
+CREATE TABLE client_ligue (id_client int PRIMARY KEY not null AUTO_INCREMENT, 
+	nom varchar(256) not null, 
+	prenom varchar(256) not null, 
+	age int not null, 
+	email varchar(256) not null)
